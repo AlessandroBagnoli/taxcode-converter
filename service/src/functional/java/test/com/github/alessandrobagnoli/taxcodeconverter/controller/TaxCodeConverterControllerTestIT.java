4d4c7234503a512c8a6700b6dbad9e13b0d923a6
Alessandro Bagnoli <alessandro.bagnoli4@gmail.com>
2022-09-10 17:28:44 +0200

    test: adjusted functional test of web layer

--- service/src/functional/java/test/com/github/alessandrobagnoli/taxcodeconverter/controller/TaxCodeConverterControllerTestIT.java
+++ service/src/functional/java/test/com/github/alessandrobagnoli/taxcodeconverter/controller/TaxCodeConverterControllerTestIT.java
@@ -2,25 +2,37 @@ package com.github.alessandrobagnoli.taxcodeconverter.controller;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.alessandrobagnoli.taxcodeconverter.dto.PersonDTO;
 import com.github.alessandrobagnoli.taxcodeconverter.dto.PersonDTO.Gender;
 import com.github.alessandrobagnoli.taxcodeconverter.dto.TaxCodeDTO;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TaxCodeConverterControllerTestIT {
 
   @Autowired
-  private TaxCodeConverterController underTest;
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
 
   @Nested
   class CalculatePersonFromTaxCodeTests {
 
+    @SneakyThrows
     @Test
     void shouldSucceedAndReturnAsExpected() {
       // given
@@ -30,9 +42,14 @@ class TaxCodeConverterControllerTestIT {
           .build();
 
       // when
-      var actual = underTest.calculatePersonFromTaxCode(input);
+      var actual = mockMvc.perform(post("/api/v1/taxcode:calculate-person-data")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(input)))
+          .andReturn()
+          .getResponse();
 
       // then
+      assertThat(actual.getStatus()).isEqualTo(HttpStatus.OK.value());
       var expected = PersonDTO.builder()
           .taxCode(taxCode)
           .gender(Gender.MALE)
@@ -41,7 +58,7 @@ class TaxCodeConverterControllerTestIT {
           .name("LSN")
           .surname("BGN")
           .build();
-      assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+      assertThat(actual.getContentAsString()).isEqualTo(objectMapper.writeValueAsString(expected));
     }
 
   }
@@ -49,6 +66,7 @@ class TaxCodeConverterControllerTestIT {
   @Nested
   class CalculateTaxCodeFromPersonTests {
 
+    @SneakyThrows
     @Test
     void shouldSucceedAndReturnAsExpected() {
       // given
@@ -61,13 +79,18 @@ class TaxCodeConverterControllerTestIT {
           .build();
 
       // when
-      var actual = underTest.calculateTaxCodeFromPerson(input);
+      var actual = mockMvc.perform(post("/api/v1/taxcode:calculate-tax-code")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(input)))
+          .andReturn()
+          .getResponse();
 
       // then
+      assertThat(actual.getStatus()).isEqualTo(HttpStatus.OK.value());
       var expected = TaxCodeDTO.builder()
           .taxCode("BGNLSN93P19H294L")
           .build();
-      assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+      assertThat(actual.getContentAsString()).isEqualTo(objectMapper.writeValueAsString(expected));
     }
 
   }
--- service/src/main/java/com/github/alessandrobagnoli/taxcodeconverter/controller/TaxCodeConverterController.java
+++ service/src/main/java/com/github/alessandrobagnoli/taxcodeconverter/controller/TaxCodeConverterController.java
@@ -4,13 +4,16 @@ import com.github.alessandrobagnoli.taxcodeconverter.dto.PersonDTO;
 import com.github.alessandrobagnoli.taxcodeconverter.dto.TaxCodeDTO;
 import com.github.alessandrobagnoli.taxcodeconverter.service.TaxCodeConverterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping(value = "/api/v1/",
+    consumes = MediaType.APPLICATION_JSON_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class TaxCodeConverterController {
 
