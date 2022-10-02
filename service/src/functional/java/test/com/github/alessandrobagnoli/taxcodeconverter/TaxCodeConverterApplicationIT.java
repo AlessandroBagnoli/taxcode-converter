7a0a6fd80fa718a0e647d006fa41867c3cdea4d3
Alessandro Bagnoli <alessandro.bagnoli4@gmail.com>
2022-10-02 16:31:39 +0200

    chore: another attempt + renamed test

--- service/src/functional/java/test/com/github/alessandrobagnoli/taxcodeconverter/TaxCodeConverterApplicationIT.java
+++ service/src/functional/java/test/com/github/alessandrobagnoli/taxcodeconverter/TaxCodeConverterApplicationIT.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-class TaxCodeConverterApplicationTest {
+class TaxCodeConverterApplicationIT {
 
   @Test
   void contextUpWithoutMocks() {
