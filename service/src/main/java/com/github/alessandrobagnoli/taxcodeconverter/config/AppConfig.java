ca18a46d2c101be40b0c230d67c1cb3633755bee
Alessandro Bagnoli <alessandro.bagnoli4@gmail.com>
2022-10-22 18:35:34 +0200

    feat: some refactor

--- service/src/main/java/com/github/alessandrobagnoli/taxcodeconverter/config/AppConfig.java
+++ service/src/main/java/com/github/alessandrobagnoli/taxcodeconverter/config/AppConfig.java
@@ -1,20 +1,34 @@
 package com.github.alessandrobagnoli.taxcodeconverter.config;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.time.Clock;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import com.github.alessandrobagnoli.taxcodeconverter.controller.TaxCodeConverterControllerAdvice.ApiError;
+import com.github.alessandrobagnoli.taxcodeconverter.dto.CalculatePersonDataRequest;
+import com.github.alessandrobagnoli.taxcodeconverter.dto.CalculatePersonDataResponse;
+import com.github.alessandrobagnoli.taxcodeconverter.dto.CalculateTaxCodeRequest;
+import com.github.alessandrobagnoli.taxcodeconverter.dto.CalculateTaxCodeResponse;
+import com.github.alessandrobagnoli.taxcodeconverter.dto.Gender;
 import com.github.alessandrobagnoli.taxcodeconverter.utils.CityCSVLoader;
 import com.github.alessandrobagnoli.taxcodeconverter.utils.CityCSVLoader.CityCSV;
 import lombok.Builder;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @Configuration
 @ImportRuntimeHints(AppRuntimeHints.class)
+@RegisterReflectionForBinding({CityCSV.class, CalculatePersonDataRequest.class, CalculatePersonDataResponse.class,
+    CalculateTaxCodeRequest.class, CalculateTaxCodeResponse.class, Gender.class, ApiError.class, Calendar[].class,
+    Date[].class, Time[].class, Timestamp[].class})
 public class AppConfig {
 
   @Bean
