3c192b46f1762b138005dba317738a8b9ffab156
Alessandro Bagnoli <alessandro.bagnoli4@gmail.com>
2022-10-22 22:13:21 +0200

    chore: cosmetics

--- service/src/main/java/com/github/alessandrobagnoli/taxcodeconverter/config/AppConfig.java
+++ service/src/main/java/com/github/alessandrobagnoli/taxcodeconverter/config/AppConfig.java
@@ -1,10 +1,6 @@
 package com.github.alessandrobagnoli.taxcodeconverter.config;
 
-import java.sql.Date;
-import java.sql.Time;
-import java.sql.Timestamp;
 import java.time.Clock;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -20,8 +16,8 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 
 @Configuration
 @ImportRuntimeHints(AppRuntimeHints.class)
-// This is needed in order to let opencsv work in native image
-@RegisterReflectionForBinding({CityCSV.class, Calendar[].class, Date[].class, Time[].class, Timestamp[].class})
+// This is needed in order to let csv parsing lib work in native image
+@RegisterReflectionForBinding({CityCSV.class})
 public class AppConfig {
 
   @Bean
